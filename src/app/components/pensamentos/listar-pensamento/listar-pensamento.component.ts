425a632e86fad8845b309d341293bd8dd294dbf9
DESKTOP/RAPHAEL <suporteti@maximaseguranca.com.br>
2024-07-30 14:14:27 -0300

    Criando Interface de Pensamentos e adaptando o projeto à esse documento.

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada pelo @Input',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis sagittis nisi. Aenean hendrerit eros odio, placerat ullamcorper leo semper sed. Nam lorem erat, ornare eget pulvinar et, lobortis id diam. Nulla sodales felis non ultricies laoreet. Pellentesque posuere iaculis odio nec dictum. Vivamus sodales tempor eleifend. Curabitur pharetra mi vitae turpis tincidunt, non viverra nunc aliquet. Nunc molestie nibh id leo ultricies, nec blandit sem auctor. Morbi faucibus ex est, non ornare orci hendrerit at. ',
-      autoria: 'Pensamento maior',
-      modelo: 'modelo1'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
