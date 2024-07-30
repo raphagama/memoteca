8f6af346c9e085151a1256b6fbd7181ad807c23d
DESKTOP/RAPHAEL <suporteti@maximaseguranca.com.br>
2024-07-30 10:45:06 -0300

    Adicinando interação entre componentes pai e filhor pelo decorator @Input

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [];
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada pelo @Input',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    }
+  ];
 
   constructor() { }
 
--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
-  pensamento = {
+  @Input() pensamento = {
     conteudo: 'I love Angular',
     autoria: 'Gama',
-    modelo: 'modelo2'
+    modelo: 'modelo1'
   }
 
   constructor() { }
